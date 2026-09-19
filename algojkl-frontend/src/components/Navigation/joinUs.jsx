6652c51db673835b0203d4abee1c30fe097bfa39
Olli Terävä <127754459+osterava@users.noreply.github.com>
2026-09-19 11:24:41 +0300

    feat: update join us button (#97)
    Changelog:
    - Update join us button to be included in i18n translations
    - Kept the old rive animation just in case, but currently no possibilities to update the animation to work with the translations so made quick button to work with the translations.
    - Added token based layout to colors, etc fonts in the css file.

--- algojkl-frontend/src/components/Navigation/joinUs.jsx
+++ algojkl-frontend/src/components/Navigation/joinUs.jsx
@@ -1,23 +1,24 @@
-import Rive from '@rive-app/react-canvas'
 import React from 'react'
-import liityjaseneksi from '../../liityjaseneksinappi.riv'
+import { useTranslation } from 'react-i18next'
 
 export default function Join() {
+  const { t } = useTranslation('common')
+
   const openLink = (url) => {
     window.open(url, '_blank')
   }
 
   return (
-    <div className="liityjaseneksiAnim">
-      <Rive
-        src={liityjaseneksi}
-        stateMachines="Hover"
-        onClick={() =>
-          openLink(
-            'https://kide.app/memberships/2a49d555-1856-4ad7-bac6-b1838e7481fc'
-          )
-        }
-      />
-    </div>
+    <button
+      type="button"
+      className="join-button-cta"
+      onClick={() =>
+        openLink(
+          'https://kide.app/memberships/2a49d555-1856-4ad7-bac6-b1838e7481fc'
+        )
+      }
+    >
+      {t('nav.joinButton')}
+    </button>
   )
 }
